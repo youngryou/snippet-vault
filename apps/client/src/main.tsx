2e35c97220ffa718ee3e0c95b39f82dafcb5f4a6
Young Ryou <ryouyt@gmail.com>
2026-06-23 14:27:01 +1200

    chore: create client app with vite react-ts

---
+++ apps/client/src/main.tsx
@@ -0,0 +1,11 @@
+// apps/client/src/main.tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
