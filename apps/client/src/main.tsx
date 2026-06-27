c72d8ae352e00b506cf56b6a9a6c041b5d1540e0
Young Ryou <ryouyt@gmail.com>
2026-06-27 20:55:07 +1200

    chore: setup feature-sliced directory structure and global styles

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,7 +1,6 @@
-// apps/client/src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
