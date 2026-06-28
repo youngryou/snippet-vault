371a78bcf0bfd4902e302b64ff16f37380a439ba
Young Ryou <ryouyt@gmail.com>
2026-06-28 14:27:45 +1200

    feat: configure data router and global query providers

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { router as routes } from './router';
 import './styles/index.css';
-import App from './App.tsx';
+
+const queryClient = new QueryClient();
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
